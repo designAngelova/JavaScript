df7cae438dd66559644a5f5f8ed3d94f1063097b
Team-Cordelia <design.angelova@gmail.com>
2014-07-14 10:10:41 +0300

    homework & demos

---
+++ JavaScript Development Introduction/js/circle-area.js
@@ -0,0 +1,5 @@
+function calcCircleArea(r){
+	var radius = prompt('Please fill the value of r:');
+	var area = Math.PI*Math.pow(radius,2);
+	document.write("r = "+ radius + "; area = "+ area+";");
+}
---
+++ JavaScript Development Introduction/js/hometown.js
@@ -0,0 +1,4 @@
+
+	var native ="Sofia";
+	alert(native);
+
